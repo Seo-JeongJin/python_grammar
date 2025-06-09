f9105ac864fcb01e90f25c07ac8896a480cbf7c7
Seo JeongJin <tj0wjd9wls8@gmail.com>
2025-06-09 13:02:04 +0900

    learned in 250609

---
+++ 0609수업.py
@@ -0,0 +1,6 @@
+
+def foo(**kwargs):
+	print(kwargs)
+
+foo(a = 2, b = 3, c = 4)
+foo(d = 70, e = 80, f = 90, g = 100)
---
+++ 0609수업2.py
@@ -0,0 +1,15 @@
+
+print((hash("2523031")))
+print((hash("2523032")))
+print((hash("2523033")))
+
+# Map (Dictionary)
+bar = {"2523031" : "홍길동", 
+       "2523032" : "김철수",
+       "2523033" : "김영희"} # key
+
+print(bar["2523031"])
+print(bar["2523032"])
+print(bar["2523033"])
+
+# -> 학번이 hash function에 들어간다.
